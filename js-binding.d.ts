753e4343fbfca874cadc73eeb3e9a3968ca50433
yisibl <percyley@qq.com>
2021-11-09 21:11:42 +0800

    chore: upgrade napi to 2.0.0-alpha.2

--- js-binding.d.ts
+++ js-binding.d.ts
@@ -1,2 +1,3 @@
 export function compress(data: Buffer): Buffer
+export function compressAsync(data: Buffer, signal?: AbortSignal | undefined | null): Promise<Buffer>
 export function decompress(buffer: Buffer): Promise<Buffer>
--- src/lib.rs
+++ src/lib.rs
@@ -2,21 +2,62 @@ use brotli;
 use napi::bindgen_prelude::*;
 use napi_derive::napi;
 
-// #[global_allocator]
-// static ALLOC: mimalloc_rust::GlobalMiMalloc = mimalloc_rust::GlobalMiMalloc;
-
-#[napi]
-pub async fn compress(buffer: Buffer) -> Buffer {
+#[inline]
+fn brotli_compress(data: &Buffer) -> Result<Vec<u8>> {
   let mut output = Vec::<u8>::new();
   let mut params = brotli::enc::BrotliEncoderParams::default();
   params.quality = 11 as i32;
 
-  match brotli::BrotliCompress(&mut buffer.as_ref(), &mut output, &params) {
+  match brotli::BrotliCompress(&mut data.as_ref(), &mut output, &params) {
     Ok(_) => (),
     Err(_) => todo!(),
   }
 
-  Buffer::from(output)
+  Ok(output) // output buffer
+}
+
+#[napi]
+pub fn compress(data: Buffer) -> Result<Buffer> {
+  let buffer = brotli_compress(&data)?;
+
+  Ok(buffer.into())
+}
+
+pub struct AsyncRenderer {
+  data: Buffer,
+}
+
+#[napi]
+impl Task for AsyncRenderer {
+  type Output = Vec<u8>;
+  type JsValue = Buffer;
+
+  fn compute(&mut self) -> Result<Self::Output> {
+    brotli_compress(&mut self.data)
+  }
+
+  fn resolve(&mut self, _env: napi::Env, result: Self::Output) -> Result<Self::JsValue> {
+    Ok(result.into())
+  }
+}
+
+#[napi]
+pub fn compress_async(
+  data: Buffer,
+  signal: Option<AbortSignal>,
+) -> AsyncTask<AsyncRenderer> {
+
+  match signal {
+    Some(s) => AsyncTask::with_signal(
+      AsyncRenderer {
+        data,
+      },
+      s,
+    ),
+    None => AsyncTask::new(AsyncRenderer {
+      data,
+    }),
+  }
 }
 
 #[napi]
